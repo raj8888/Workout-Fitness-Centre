4bed381807aa3f8df172063b49b2c4cb7d1e26fd
sarveshgupta1997 <sarveshgupta14@gmail.com>
2023-03-29 12:25:45 +0530

    nodemon file changed

--- Backend/config/server.js
+++ Backend/config/server.js
@@ -1,7 +1,8 @@
-const mongoose=require('mongoose')
-require('dotenv').config()
-mongoose.set('strictQuery',false)
-const connection=mongoose.connect(process.env.mongourl)
+const mongoose=require('mongoose');
+require('dotenv').config();
+mongoose.set('strictQuery',false);
+
+const connection=mongoose.connect(process.env.mongourl);
 
 module.exports={
     connection
