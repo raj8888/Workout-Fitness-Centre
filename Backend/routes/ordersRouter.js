075c6966edc7c65b3359358ee307d9429dc5c999
ajayjamage3 <ajayjamage3@gmail.com>
2023-04-02 12:08:11 +0530

    ui updated

--- Backend/routes/ordersRouter.js
+++ Backend/routes/ordersRouter.js
@@ -89,35 +89,6 @@ ordersRouter.post("/create", async (req,res)=>{
     }
 })
 
-// ordersRouter.post("/create", async (req,res)=>{
-//     let payload = req.body;
-//     payload.status=true;
-//     payload.createdDate=get_date();
-//     payload.createdTime=get_time();
-//     let classID = payload.classID;
-//     try{
-//         let classes = await ClassesModel.findOne({_id:classID});
-//         console.log(classes,classID)
-//         if(classes.clients.includes(payload.userID)){            
-//             res.status(401).send({message:"You have already registered for this class"})
-//         }else{
-//             if(classes.seatOccupied < classes.seatTotal){
-//                 let order = new OrdersModel(payload);
-//                 await order.save();                
-//                 await ClassesModel.findByIdAndUpdate({_id:classID},{seatOccupied:classes.seatOccupied+1,clients:[...classes.clients,payload.userID]}) // increment seats occupied
-//                 await UserModel.findByIdAndUpdate({_id:payload.userID},{ $push: { classes: classes._id } });
-//                 res.status(200).send({message:"Order created",order})
-//             }else{
-//                 res.status(401).send({message:"All seats are Booked"})
-//             }
-//         }
-//     }catch(error){
-//         res.status(400).send({message:"Something went wrong",error:error.message})
-//     }
-// })
-
-
-// Order status Update (true or false)
 ordersRouter.patch("/update/:id", async (req,res)=>{
     let orderID= req.params.id;
     try{
